fdbc8db64eac6b6e1d1432ce3ec47badeb2ea051
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-05-06 16:57:30 -0500

    feat: Implement builder pattern for Component
    This allows us to make a component like this:
    
    ComponentBuilder::new(0, 0, 100, 100)
        .height(100)
        .left_color("red")
        .right_color("blue")
        .left_text("Hello")
        .right_text("World!")
        .build()

---
+++ src/component_builder.rs
@@ -0,0 +1,43 @@
+use crate::component::Component;
+use crate::point::Point;
+
+pub struct ComponentBuilder {
+    component: Component,
+}
+
+impl ComponentBuilder {
+    pub fn new(x1: f64, y1: f64, x2: f64, y2: f64) -> Self {
+        Self {
+            component: Component::new(Point::new(x1, y1), Point::new(x2, y2)),
+        }
+    }
+
+    pub fn left_color(mut self, color: &str) -> ComponentBuilder {
+        self.component.set_color1(color.to_string());
+        self
+    }
+
+    pub fn right_color(mut self, color: &str) -> ComponentBuilder {
+        self.component.set_color2(color.to_string());
+        self
+    }
+
+    pub fn left_text(mut self, text: &str) -> ComponentBuilder {
+        self.component.set_text1(text.to_string());
+        self
+    }
+
+    pub fn right_text(mut self, text: &str) -> ComponentBuilder {
+        self.component.set_text2(text.to_string());
+        self
+    }
+
+    pub fn height(mut self, height: f64) -> ComponentBuilder {
+        self.component.set_bar_height(height);
+        self
+    }
+
+    pub fn build(self) -> Component {
+        self.component
+    }
+}
