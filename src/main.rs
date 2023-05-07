9ecbfe04d9040192a6c7bf57cb546efa9a240518
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-05-07 11:36:00 -0500

    refactor: Move parsing and rendering logic into functions
    This logic was previously just crammed into `main`. This refactor allows
    me to embed multiple SVGs into the document body.

--- src/main.rs
+++ src/main.rs
@@ -19,58 +19,9 @@ fn parse_line(line: &str) -> (i64, String) {
     (epoch, tag.to_string())
 }
 
-fn main() {
-    let current_time = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .unwrap()
-        .as_secs() as i64;
+fn render_tree(tree: &TreeNode, id: &str) -> String {
+    let mut svg = format!("<svg id={id} style='display: none' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'>\n");
 
-    let mut activities = Vec::new();
-
-    let mut file = File::open("log").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let mut lines = contents.lines();
-
-    let mut last_line = parse_line(lines.next().unwrap());
-    let time_period = 60 * 60 * 24;
-
-    while let Some(line) = lines.next() {
-        let contents = parse_line(line);
-
-        let delta = contents.0 - last_line.0;
-
-        if contents.0 < current_time - time_period || last_line.1 == "health.rest.sleep" {
-        } else {
-            activities.push((delta, last_line.1));
-        }
-        last_line = contents;
-    }
-
-    let epoch = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .unwrap()
-        .as_secs() as i64;
-    let delta = epoch - last_line.0;
-    activities.push((delta, last_line.1));
-
-    let mut tree = TreeNode {
-        value: 0.0,
-        children: HashMap::new(),
-    };
-
-    for activity in &activities {
-        let time = activity.0 as f64;
-        let major = activity.1.split('.').nth(0).unwrap();
-        let minor = activity.1.split('.').nth(1).unwrap();
-        let activity = activity.1.split('.').nth(2).unwrap();
-        tree.insert(major, minor, activity, time);
-        println!("{} | {} | {} | {}", major, minor, activity, time);
-    }
-
-    let mut svg = format!("<svg width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'>\n");
-
-    println!("{}", tree.value);
     let mut y = 10.;
     let factor = tree.value / 700.;
     let width = 1870. / 3.;
@@ -157,7 +108,83 @@ fn main() {
         outercount += step;
     }
 
-    svg += "</svg>\n";
+    svg + "</svg>\n"
+}
+
+fn parse_file(filename: &str, time_period: i64) -> TreeNode {
+    let current_time = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs() as i64;
+
+    let mut activities = Vec::new();
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut lines = contents.lines();
+
+    let mut last_line = parse_line(lines.next().unwrap());
+
+    while let Some(line) = lines.next() {
+        let contents = parse_line(line);
+
+        let delta = contents.0 - last_line.0;
+
+        if contents.0 < current_time - time_period || last_line.1 == "health.rest.sleep" {
+        } else {
+            activities.push((delta, last_line.1));
+        }
+        last_line = contents;
+    }
+
+    let epoch = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs() as i64;
+    let delta = epoch - last_line.0;
+    activities.push((delta, last_line.1));
+
+    let mut tree = TreeNode {
+        value: 0.0,
+        children: HashMap::new(),
+    };
+
+    for activity in &activities {
+        let time = activity.0 as f64;
+        let major = activity.1.split('.').nth(0).unwrap();
+        let minor = activity.1.split('.').nth(1).unwrap();
+        let activity = activity.1.split('.').nth(2).unwrap();
+        tree.insert(major, minor, activity, time);
+        println!("{} | {} | {} | {}", major, minor, activity, time);
+    }
+
+    tree
+}
+
+fn main() {
+    let mut svg=String::new();
+
+    let tree = parse_file("/home/sam/rofi_time_tracker/log", 60*60*24*365);
+    svg+=render_tree(&tree, "graph-yearly").as_str();
+
+    let tree = parse_file("/home/sam/rofi_time_tracker/log", 60*60*24*30);
+    svg+=render_tree(&tree, "graph-monthly").as_str();
+
+    let tree = parse_file("/home/sam/rofi_time_tracker/log", 60*60*24*7);
+    svg+=render_tree(&tree, "graph-weekly").as_str();
+
+    let tree = parse_file("/home/sam/rofi_time_tracker/log", 60*60*24);
+    svg+=render_tree(&tree, "graph-24-hours").as_str();
+
+    let tree = parse_file("/home/sam/rofi_time_tracker/log", 60*60*12);
+    svg+=render_tree(&tree, "graph-12-hours").as_str();
+
+    let tree = parse_file("/home/sam/rofi_time_tracker/log", 60*60*6);
+    svg+=render_tree(&tree, "graph-6-hours").as_str();
+
+    let tree = parse_file("/home/sam/rofi_time_tracker/log", 60*60*1);
+    svg+=render_tree(&tree, "graph-1-hours").as_str();
 
     let output = include_str!("template.html");
     std::fs::write("index.html", output.replace("BODY", svg.as_str())).unwrap();
