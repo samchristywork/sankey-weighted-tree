052a1a070c0489558110787d60d5d313fe4dd0c2
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-05-06 21:14:27 -0500

    feat: Add main body of program
    The main function contains the logic that ingests the log file, parses
    it, and then constructs a Sankey diagram based on that data.

---
+++ src/main.rs
@@ -0,0 +1,127 @@
+pub mod component;
+pub mod component_builder;
+pub mod point;
+pub mod tree_node;
+
+use component_builder::ComponentBuilder;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
+use tree_node::TreeNode;
+
+fn parse_line(line: &str) -> (i64, String) {
+    let mut words = line.split('\t');
+    let epoch: i64 = words.next().unwrap().parse().unwrap();
+    let tag = words.next().unwrap();
+
+    (epoch, tag.to_string())
+}
+
+fn main() {
+    let mut activities = Vec::new();
+
+    let mut file = File::open("log").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut lines = contents.lines();
+
+    let mut last_line = parse_line(lines.next().unwrap());
+
+    while let Some(line) = lines.next() {
+        let contents = parse_line(line);
+
+        let delta = contents.0 - last_line.0;
+
+        activities.push((delta, last_line.1));
+        last_line = contents;
+    }
+
+    let epoch = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs() as i64;
+    let delta = epoch - last_line.0;
+    activities.push((delta, last_line.1));
+
+    let mut tree=TreeNode{value:0.0, children:HashMap::new()};
+
+    for activity in &activities {
+        let time = activity.0 as f64;
+        let major = activity.1.split('.').nth(0).unwrap();
+        let minor = activity.1.split('.').nth(1).unwrap();
+        let activity = activity.1.split('.').nth(2).unwrap();
+        tree.insert(major, minor, activity, time);
+        println!("{} | {} | {} | {}", major, minor, activity, time);
+    }
+
+    let mut svg = format!(
+        "<svg width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'>\n"
+    );
+
+    println!("{}", tree.value);
+    let mut y=10.;
+    let factor=tree.value/700.;
+    let width=1870./3.;
+    let mut innercount=0.;
+    let mut middlecount=0.;
+    let mut outercount=0.;
+
+    for key in tree.children.keys() {
+        let x=10.;
+
+        let value = tree.children[key].value / factor;
+        println!("{} {}", key, value);
+
+        svg += ComponentBuilder::new(x, y, x+width-10., y+outercount)
+            .height(value)
+            .left_color("red")
+            .right_color("grey")
+            .right_text(key)
+            .build()
+            .draw()
+            .as_str();
+
+        let tree=&tree.children[key];
+        for key in tree.children.keys() {
+            let x=x+width;
+
+            let value = tree.children[key].value / factor;
+            println!("  {} {}", key, value);
+
+            svg += ComponentBuilder::new(x, y+outercount, x+width-10., y+middlecount)
+                .height(value)
+                .left_color("red")
+                .right_color("grey")
+                .right_text(key)
+                .build()
+                .draw()
+                .as_str();
+
+            let tree=&tree.children[key];
+            for key in tree.children.keys() {
+                let x=x+width;
+
+                let value = tree.children[key].value / factor;
+                println!("    {} {}", key, value);
+
+                svg += ComponentBuilder::new(x, y+middlecount, x+width-10., y+innercount)
+                    .height(value)
+                    .left_color("red")
+                    .right_color("grey")
+                    .right_text(key)
+                    .build()
+                    .draw()
+                    .as_str();
+                y+=value;
+                innercount+=10.;
+            }
+            middlecount+=10.;
+        }
+        outercount+=10.;
+    }
+
+    svg += "</svg>\n";
+
+    let output = include_str!("template.html");
+    std::fs::write("index.html", output.replace("BODY", svg.as_str())).unwrap();
+}
